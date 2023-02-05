9f9f10a2a1c66b507fa3daa066877e0ffd5f3b55
greenboxspb <green.box.spb@gmail.com>
2023-02-05 22:02:32 +0300

    create superuser

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE(collector, field, sub_objs, using))
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
